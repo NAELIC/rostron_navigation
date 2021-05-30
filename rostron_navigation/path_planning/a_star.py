d2156401485d152908390149716934bb5139b40b
Etienne Schmitz <contact@etienne-schmitz.com>
2021-05-30 21:33:24 +0200

    clean to make step 1

--- rostron_navigation/path_planning/a_star.py
+++ rostron_navigation/path_planning/a_star.py
@@ -1,20 +1,46 @@
-import numpy
+import math
+import numpy as np
 
-# TODO Chercher un nom pour la classe mère
+from rostron_interfaces.action import MoveTo
+from rostron_utils.world import World
+
+# TODO(Etienne) Chercher un nom pour la classe mère
 class AStar():
 
-    def __init__(self) -> None:
-        self.create_grid()
-        pass
+    def __init__(self, id: int, goal : MoveTo.Goal) -> None:
+        resolution = 0.1
+        margin = 3*2
+        self.x_ = math.ceil(World().field.width / resolution) + margin
+        self.y_ = math.ceil(World().field.length / resolution) + margin
+
+        self.id_ = id
+        self.goal_ = goal
     
     def create_grid(self):
-        # keep field 
-        field_length = 10.4
-        field_width = 7.4
-        resolution = 0.1
+        grid = np.zeros((self.y_, self.x_))
 
-        grid_x = field_width / resolution
-        grid_y = field_length / resolution
-        # np.zeros((grid_x, grid_y))
-        numpy.zeros((grid_x, grid_y))
-        pass
+        """
+        1. Parcourir l'ensemble des alliées et des opposants.
+        Durée : Une demi-journée maximum
+           
+        Ajouter ces obstacles au niveau de la grille crée en haut.
+        0 -> libre
+        2 -> obstacle (impossible d'y passer)
+        
+        Il faut convertir les coordonées du robot, dans la grille.
+        Si tu ne comprends pas comment faire, je te conseille de poser sur un dessin.
+        En y réfléchissant j'ai vu une translation puis un -y.
+        (x_robot, y_robot) -> (x_grid, y_grid)
+        Cette transformation doit être facilement changeable en fonction des données du terrain (pas de valeur en dure !)
+
+        /!\ Si l'id de l'allié est le même, ... (je te laisse deviner)
+        /!\ J'ai ajouté des marges en x et en y
+
+        2. Convertir la grille numpy en une map sous ROS affichable sous RVIZ via un topic map?.
+        Durée : Une demi-journée 
+        Je te laisse chercher le type, etc....
+        Y a un type pour ça dans common_msgs de ROS2.
+        """
+
+    def run(self):
+        self.create_grid()
--- rostron_navigation/primitive/move_to.py
+++ rostron_navigation/primitive/move_to.py
@@ -1,16 +1,10 @@
-import math
-
-import rclpy
 from rclpy.action.server import ServerGoalHandle
-from rclpy.node import Node
 from geometry_msgs.msg import Point
 from rostron_interfaces.action import MoveTo
-from rostron_interfaces.msg import Robots, Order
 from rostron_utils.world import World
 
 from .primitive import Primitive
-from ..path_planning.aStar_to_ros import AStar
-from ..rviz_vizualisation import RvizVizualisation
+from ..path_planning.a_star import AStar
 
 
 class MoveToPrimitive(Primitive):
@@ -19,105 +13,13 @@ class MoveToPrimitive(Primitive):
         self.id = id
 
     def handler_callback(self, goal_handle: ServerGoalHandle) -> None:
-        pose : Point = goal_handle.request.position
-        orientation : float = goal_handle.request.orientation
+        goal : MoveTo.Goal = goal_handle.request
         World().node_.get_logger().info('[MOVETO] - Executing MoveTo (%.2lf, %.2lf, %.2lf)' % (
-            pose.x, pose.y, orientation))
-        # self.node.get_logger().info('Executing MoveTo goal...')
-        # self.node.get_logger().info('%lf' % pose.position.x)
-        # print("Destination :", (pose.position.x,pose.position.y))
+            goal.position.x, goal.position.y, goal.orientation))
 
-        # robot = MoveToStrategie()
-        # a = AStar(self.id,(pose.position.x,pose.position.y), 0.0)
-        # path= a.run()
-        # robot.move_to(self.id,path)
-
-        # RvizVizualisation().get_rviz_vizualisation(path)
-
+        a = AStar(self.id, goal)
+        a.run()
+        
         goal_handle.succeed()
         result = MoveTo.Result()
         return result
-
-# class MoveToStrategie(Node):
-#     def __init__(self):
-#         super().__init__("move_to")
-#         self.subscription = self.create_subscription(
-#             Robots,
-#             '/yellow/allies',
-#             self.listener_robots,
-#             1
-#         )
-#         self.subscription
-#         self.poseRobots = Robots()
-#         self.publisher = self.create_publisher(Order,'/yellow/order', 1)
-#         self.finished=False
-
-#     def listener_robots(self, robots):
-#         self.poseRobots = robots
-#         return robots
-
-#     def distance (self, x, y, xa, ya):
-#         return math.sqrt(((xa-x)**2)+((ya-y)**2))
-
-#     """
-#         Regarder tf de ros....
-#         Numpy propose sinon quelque chose dans cette veine !
-#     """
-#     def matriceRotation(self, x ,y, theta):
-#         # [cos(θ) -sin(θ)]   [x]
-#         # [sin(θ)  cos(θ)] * [y]
-#         return( cos(theta)*x - sin(theta)*y , sin(theta)*x+cos(theta)*y )
-
-#     def move_to(self, id, path):
-#         """Move to the last pose of the path going through all poses"""
-#         for pose in path:
-#             rclpy.spin_once(self)
-#             robotIdPose = self.poseRobots.robots[id].pose # Robot 0
-
-#             ArriveX=pose[0]
-#             ArriveY=pose[1]
-
-#             # 1. récupérer: position et orientation courante du robot + position d'arrivé les deux étant dans le repère du terrain
-#             robotX = robotIdPose.position.x
-#             robotY = robotIdPose.position.y
-#             robotO = robotIdPose.orientation.z
-
-#             # 2. calculer le vecteur allant de la position du robot à la position d'arrivée
-#             vecteur = (ArriveX-robotX, ArriveY-robotY)
-
-#             # 3. créer une matrice de rotation anti-horaire correspondant à l'orientation du robot
-#             # 4. appliquer cette matrice sur le vecteur
-#             vecteur = self.matriceRotation(vecteur[0],vecteur[1],robotO)
-
-#             robotX = self.poseRobots.robots[id].pose.position.x
-#             robotY = self.poseRobots.robots[id].pose.position.y
-
-#             while self.distance(robotX, robotY, ArriveX, ArriveY)>0.1 :
-#                 rclpy.spin_once(self) # get current poseRobots
-#                 robotX = self.poseRobots.robots[id].pose.position.x
-#                 robotY = self.poseRobots.robots[id].pose.position.y
-
-#                 if self.distance(robotX, robotY, ArriveX, ArriveY)>0.3 or pose==path[-1]:
-#                     # solution pour que le robot ne s'arrête pas à chaque pose
-#                     # à 30cm de sa destination il garde le même vecteur et donc ne ralentit pas
-#                     # exception pour le dernier pose où il ralentit
-#                     vecteur = (ArriveX-robotX, ArriveY-robotY)
-#                     vecteur = self.matriceRotation(vecteur[0],vecteur[1],robotO)
-
-#                 vel_msg = Twist()
-#                 vel_msg.linear.x= vecteur[0]
-#                 vel_msg.linear.y= vecteur[1]
-#                 msg = Order()
-#                 msg.id = id
-#                 msg.velocity = vel_msg
-#                 self.publisher.publish(msg)
-#         self.stop_robot(id)
-
-#     def stop_robot(self, id):
-#         """Give at the robot a nullable velocity"""
-#         msg = Order()
-#         msg.id = id
-#         msg.velocity = Twist()
-#         self.publisher.publish(msg)
-#         print("Destination OK")
-#         self.finished=True
