babb96813e309a23ff96a3561565a9c2fe9e25b6
ValentinLUC <valentin.luc@esme.fr>
2022-01-19 15:09:03 +0100

    a bit improved

--- rostron_navigation/joysticks.py
+++ rostron_navigation/joysticks.py
@@ -1,42 +1,30 @@
-import json, os, pygame
+import json, os, pygame, time
+import matplotlib.pyplot as plt
 
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from rostron_interfaces.action import Behavior
+from rostron_interfaces.msg import Order, Orders, Hardware
+from geometry_msgs.msg import Twist
 from rostron_interfaces.msg import Commands, Command, Hardware, Robot
 from rostron_ia_ms.strategies.control import Control
 from rostron_ia_ms.strategies.go_to import GoTo
-from controller_movements import Controller
+# from controller_movements import Controller
 from rostron_navigation.main import RobotNavigation
+from rostron_navigation.primitive.move_to import MoveTo
 from rostron_utils.world import World
 
-def create_move_to(x, y, theta):
-    msg = Behavior.Goal()
-
-    msg.name = "move_to"
-    msg.params = json.dumps({"x": x, "y": y, "theta": theta})
-
-    return msg
+speed_multiplier = 10
 
 class Joysticks(Node):
-    tasks = []
     ################################# LOAD UP A BASIC WINDOW #################################
-    def __init__(self) -> None :
-        pygame.init()
-        ###
-        self.client_ = ActionClient(World().node_, Behavior, f"robot_{id}/behavior")
-        self.client_.wait_for_server()
-        
-        self.create_timer(0.16, self.update)
+    def __init__(self, id: int) -> None :
+        self.id = id
+        # self.publisher = World().node_.create_publisher(Orders, 'joysticks', 1)
 
-        self.declare_parameter('yellow', True)
-        self.is_yellow = self.get_parameter('yellow').get_parameter_value().bool_value
-        thisRobot = World().allies[0]
-        World().init(self, self.is_yellow)
-        # self.client_.send_goal_async(create_move_to(0.0+self.player.x, 0.0+self.player.y, 0.0))
-        ###
+        pygame.init()
         DISPLAY_W, DISPLAY_H = 960, 570
         canvas = pygame.Surface((DISPLAY_W,DISPLAY_H))
         window = pygame.display.set_mode(((DISPLAY_W,DISPLAY_H)))
@@ -46,6 +34,12 @@ class Joysticks(Node):
         clock = pygame.time.Clock()
         pygame.display.set_caption("Joysticks Movement")
         color = 0
+
+        # World.init(self)
+        # self.robot_position = (World().allies[self.id].pose.position.x,
+        #                     World().allies[self.id].pose.position.y)
+        # self.current_location=(self.robot_position, time.time())
+        self.speed_over_time=[[0.0, 0.0]] #  [[speed,time],...]
         ###########################################################################################
 
         #Initialize controller
@@ -59,7 +53,7 @@ class Joysticks(Node):
             button_keys = json.load(file)
         # 0: Left analog horizonal, 1: Left Analog Vertical, 2: Right Analog Horizontal
         # 3: Right Analog Vertical 4: Left Trigger, 5: Right Trigger
-        analog_keys = {0:0, 1:0, 2:0, 3:0, 4:-1, 5: -1 }
+        analog_keys = {0:0, 1:0, 2:0, 3:0, 4:-1, 5:-1}
 
         # START OF GAME LOOP
         while running:
@@ -67,6 +61,7 @@ class Joysticks(Node):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                    self.plot_speed_over_time()
                 if event.type == pygame.KEYDOWN:
                     ############### UPDATE SPRITE IF SPACE IS PRESSED #################################
                     pass
@@ -81,30 +76,30 @@ class Joysticks(Node):
                         DOWN = True
                     if event.button == button_keys['up_arrow']:
                         UP = True
+                    if event.button == button_keys['circle']:
+                        UP, DOWN, LEFT, RIGHT = False, False, False, False  
+                        print("Stop!")
+
                 # HANDLES BUTTON RELEASES
                 if event.type == pygame.JOYBUTTONUP:
                     if event.button == button_keys['left_arrow']:
                         LEFT = False
-                        # os.system('ros2 topic pub /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : 0, velocity: { linear: { x: 0.0, y: 0 }, angular: { z: 0.0 }}}]}"')
                     if event.button == button_keys['right_arrow']:
                         RIGHT = False
-                        # os.system('ros2 topic pub /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : 0, velocity: { linear: { x: 0.0, y: 0 }, angular: { z: 0.0 }}}]}"')
                     if event.button == button_keys['down_arrow']:
                         DOWN = False
-                        # os.system('ros2 topic pub /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : 0, velocity: { linear: { x: 0.0, y: 0 }, angular: { z: 0.0 }}}]}"')
                     if event.button == button_keys['up_arrow']:
                         UP = False
-                        # os.system('ros2 topic pub /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : 0, velocity: { linear: { x: 0.0, y: 0 }, angular: { z: 0.0 }}}]}"')
 
-                #HANDLES ANALOG INPUTS
+                # HANDLES ANALOG INPUTS
                 if event.type == pygame.JOYAXISMOTION:
                     analog_keys[event.axis] = event.value
-                    # print(analog_keys)
+                    # os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: ' + str(speed_multiplier*analog_keys[0]) + ', y: ' + str(speed_multiplier*analog_keys[1]) + '}, angular: { z: 0.0 }}}]}"')
+                    # print(analog_keys) 
                     # Horizontal Analog
-                    if abs(analog_keys[0]) > 0.4:
+                    if abs(analog_keys[0]) > 0.5:
                         if analog_keys[0] < -0.7:
                             LEFT = True
-                            
                         else:
                             LEFT = False
                         if analog_keys[0] > 0.7:
@@ -112,7 +107,7 @@ class Joysticks(Node):
                         else:
                             RIGHT = False
                     # Vertical Analog
-                    if abs(analog_keys[1]) > 0.4:
+                    if abs(analog_keys[1]) > 0.5:
                         if analog_keys[1] < -0.7:
                             UP = True
                         else:
@@ -127,24 +122,79 @@ class Joysticks(Node):
                     if analog_keys[5] > 0:  # Right Trigger
                         color -= 2
 
+                if event.type == pygame.JOYHATMOTION: # Doesn't work yet
+                    """ Control with the arrows of the controller """
+
+                    # get the position of a joystick hat
+                    # get_hat(hat_number) -> x, y
+                    # Returns the current position of a position hat. The position is given as two values representing the x and y position for the hat. 
+                    # (-1, 0) means left; 
+                    # (1, 0) means right;
+                    # (0, 1) means up; 
+
+                    hats = joystick.get_numhats()
+                    for i in range(hats):
+                        hat = joystick.get_hat(i)
+
+                    if hat == (-1, 0):
+                        LEFT = True
+                        print("Go left!")
+                        os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 0.0, y: 1 }, angular: { z: 0.0 }}}]}"')
+                    elif hat == (1, 0):
+                        RIGHT = True
+                        print("Go right!")
+                        os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 0.0, y: -1 }, angular: { z: 0.0 }}}]}"')
+                    elif hat == (0, 1):
+                        UP = True
+                        print("Go up!")
+                        os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 1, y: 0 }, angular: { z: 0.0 }}}]}"')
+                    elif hat == (0, -1):
+                        DOWN = True
+                        print("Go down!")
+                        os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: -1, y: 0 }, angular: { z: 0.0 }}}]}"')
+
             # Handle Player movement
             if LEFT:
-                player.x -=5
-                # print(World.allies[0].pose.position.x)
+                # player.x -= 5
                 print("Go left!")
-                # self.tasks.append(Controller(0, World.allies[0].pose.position.x-0.5, 0.0, 0.0))
-                self.tasks.append(GoTo(0, -0.5, 0.0, 0.0))
+                # print("Coordonnées : " + str(player.x) + " " + str(player.y))
+                os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 0.0, y: ' + str(speed_multiplier*(-analog_keys[0])) + ' }, angular: { z: 0.0 }}}]}"')
             if RIGHT:
-                player.x += 5
+                # player.x += 5
                 print("Go right!")
+                # print("Coordonnées : " + str(player.x) + " " + str(player.y))
+                os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 0.0, y: '  + str(speed_multiplier*(-analog_keys[0])) + ' }, angular: { z: 0.0 }}}]}"')
                 
             if UP:
-                player.y -= 5
+                # player.y -= 5
                 print("Go up!")
+                # print("Coordonnées : " + str(player.x) + " " + str(player.y))
+                os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: ' + str(speed_multiplier*(-analog_keys[1])) + ', y: 0 }, angular: { z: 0.0 }}}]}"')
                 
             if DOWN:
-                player.y += 5
+                # player.y += 5
                 print("Go down!")
+                # print("Coordonnées : " + str(player.x) + " " + str(player.y))
+                os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: ' + str(speed_multiplier*(-analog_keys[1])) + ', y: 0 }, angular: { z: 0.0 }}}]}"')
+                
+                ##########################################
+                # control = Order()
+                # control.id = self.id
+                
+                # vel_msg = Twist()
+                # vel_msg.linear.x = speed_multiplier*(-analog_keys[1])
+                # vel_msg.linear.y = 0.0
+                # control.velocity = vel_msg
+                
+                # msg = Orders()
+                # msg.orders.append(control)
+                # self.publisher.publish(msg)
+                
+                # rclpy.spin_once(self.node) 
+                ##########################################
+            if (LEFT==False and RIGHT==False and UP==False and DOWN==False):
+                # self.stop_robot()
+                os.system('ros2 topic pub --once /yellow/commands rostron_interfaces/msg/Commands "{ commands : [{ id : ' + str(id) + ' , velocity: { linear: { x: 0.0, y: 0.0 }, angular: { z: 0.0 }}}]}"')
                 
             if color < 0:
                 color = 0
@@ -155,19 +205,47 @@ class Joysticks(Node):
             canvas.fill((255,255,255))
             pygame.draw.rect(canvas, (0,0 + color,255), player)
             window.blit(canvas, (0,0))
-            clock.tick(60)
+            clock.tick(10)
             pygame.display.update()
 
-    def update(self):
-        for t in self.tasks:
-            t.update()
+    def stop_robot(self):
+        """Give to the robot a nullable velocity"""     
+        control = Order()
+        control.id = self.id
+        control.velocity = Twist()
 
-# def main():
-#     rclpy.init()
-#     node = Joysticks()
-#     rclpy.spin(node)
+        msg = Orders()
+        msg.orders.append(control)
+        self.publisher.publish(msg)
+            
+    def speed_checker(self):
+        last_location=self.current_location
+        self.current_location=(self.robot_position, time.time())
+        distance = self.distance(last_location[0],self.current_location[0])
+        tim = self.current_location[1]- last_location[1]
+        speed = distance/tim
+        self.speed_over_time.append([speed,self.speed_over_time[-1][1]+tim])
 
-#     node.destroy_node()
-#     rclpy.shutdown()
+    def plot_speed_over_time(self):
+        speed = [i[0] for i in self.speed_over_time]
+        time  = [i[1] for i in self.speed_over_time]
+        plt.plot(time, speed, color='blue')
+        plt.xlabel('Time (in sec)')
+        plt.ylabel('Speed (in m/s)')
 
-# main()
+        average_speed= round(sum(speed)/len(speed),1)
+        total_time= round(time[-1],1)
+        distance = round(sum(speed)/len(speed)*time[-1],1)
+        title= "Robot %i Path | Distance: %.1lfm | Time: %.1lfs | Average Speed: %.1lfm/s" % (self.id, distance, total_time, average_speed)
+        plt.title(title)
+        plt.show()
+
+def main():
+    rclpy.init()
+    node = Joysticks(5)
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+main()
